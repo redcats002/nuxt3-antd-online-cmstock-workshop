1e3b403c6af7131481ef4a25383ebecffa91dd48
redcats002 <redcats_002@hotmail.com>
2023-06-09 18:08:20 +0700

    :package: setup dependencies
    vueuse, tailwindcss, antd

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,20 @@
+import { resolve } from "path";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  plugins: ['~/plugins/antd.ts'],
+  alias: {
+    "@": resolve(__dirname, "/")
+  },
+  css: ["~/assets/main.css", "~/assets/tailwind.css"],
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    exposeLevel: 2,
+    injectPosition: 'first',
+    viewer: true,
+  }
 })
