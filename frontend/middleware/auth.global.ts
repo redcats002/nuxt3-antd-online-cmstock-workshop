21b0b3aed21f9b19ec56221e47f2da39ca1ac290
redcats002 <redcats_002@hotmail.com>
2023-06-14 18:07:54 +0700

    :construction: global middleware

--- frontend/middleware/auth.global.ts
+++ frontend/middleware/auth.global.ts
@@ -1,13 +1,33 @@
+import { useAuth } from "~/stores/useAuth"
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    const { fetch, isLoading } = useFetcher()
-    setAuthorizationHeader('YOUR_TOKEN_HERE')
-    const loggedIn = true
+    // const router = useRouter()
+    const authStore = useAuth()
+    // console.log(authStore.session.isLoggedIn)
+    to.matched.some((record) => (record.meta.layout == 'default' ? true : false))
 
-    if (to.path === '/login' && !loggedIn) {
-        abortNavigation("You MUST be logged in")
-
+    if (to.meta.layout === 'default') {
+        console.log('secure route')
+        // secure route
+        if (authStore.session.isLoggedIn) {
+            abortNavigation()
+        } else {
+            console.log('not logged in')
+            // navigateTo('/login');
+        }
+    } else {
+        // unsecure route
+        console.log('unsecure route')
+        if (authStore.session.isLoggedIn) {
+            // router.push('/stock')
+            // navigateTo('/stock');
+        } else {
+            console.log('not logged in')
+            // abortNavigation()
+        }
     }
+
+
 })
 
 
