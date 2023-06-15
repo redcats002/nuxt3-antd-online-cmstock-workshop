0b7a836bc5bb48bb9b8ab97729133d0a686fb6ec
redcats002 <redcats_002@hotmail.com>
2023-06-15 18:13:36 +0700

    :art: layout on report page

--- frontend/pages/report.vue
+++ frontend/pages/report.vue
@@ -1,4 +1,27 @@
 <template>
-  <div></div>
+  <a-row class="tw-p-5" :gutter="[0, 10]">
+    <a-col :span="24">
+      <a-card
+        class="tw-w-full tw-h-[400px] tw-drop-shadow-md hover:tw-drop-shadow-md tw-transition-all tw-rounded-lg"
+      >
+      </a-card>
+    </a-col>
+    <a-col :span="16" class="tw-pr-3">
+      <a-card
+        class="tw-w-full tw-h-[300px] tw-drop-shadow-md hover:tw-drop-shadow-md tw-transition-all tw-rounded-lg"
+      >
+      </a-card>
+    </a-col>
+    <a-col :span="8">
+      <a-card
+        class="tw-w-full tw-h-[300px] tw-drop-shadow-md hover:tw-drop-shadow-md tw-transition-all tw-rounded-lg"
+      >
+      </a-card>
+    </a-col>
+  </a-row>
 </template>
-<script lang="ts"></script>
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'default',
+});
+</script>
