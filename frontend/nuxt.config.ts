52f8a5381265b819065f12943b4ea5dcc7c1f71b
redcats002 <redcats_002@hotmail.com>
2023-06-19 13:47:30 +0700

    :wrench: Config chartjs, vuechart3

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,35 +3,36 @@ import { resolve } from "path";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@pinia/nuxt', 'dayjs-nuxt'],
-  plugins: ['~/plugins/antd.ts'],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@pinia/nuxt", "dayjs-nuxt"],
+  plugins: ["~/plugins/chartjs.client.ts", "~/plugins/antd.ts"],
   alias: {
-    "@": resolve(__dirname, "/")
+    "@": resolve(__dirname, "/"),
   },
   css: ["~/assets/main.css", "~/assets/tailwind.css"],
+
   tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-    configPath: 'tailwind.config.js',
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config.js",
     exposeConfig: false,
     exposeLevel: 2,
-    injectPosition: 'first',
+    injectPosition: "first",
     viewer: true,
   },
   pinia: {
     autoImports: [
       // automatically imports `defineStore`
-      'defineStore', // import { defineStore } from 'pinia'
-      ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+      "defineStore", // import { defineStore } from 'pinia'
+      ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
     ],
   },
   imports: {
     dirs: [
       // Scan top-level modules
-      'composables',
+      "composables",
       // ... or scan modules nested one level deep with a specific name and file extension
-      'composables/*/index.{ts,js,mjs,mts}',
+      "composables/*/index.{ts,js,mjs,mts}",
       // ... or scan all modules within given directory
-      'composables/**'
-    ]
-  }
-})
+      "composables/**",
+    ],
+  },
+});
