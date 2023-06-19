808e6ad3652fd8f21e848351e1b5baf50c617cc2
redcats002 <redcats_002@hotmail.com>
2023-06-19 18:19:42 +0700

    :coffin: remove method deadcode

--- frontend/middleware/auth.global.ts
+++ frontend/middleware/auth.global.ts
@@ -1,90 +1,24 @@
-import { useAuth } from "~/stores/useAuth"
+import { useAuth } from "~/stores/useAuth";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
+  console.log("client-middleware-global");
+  const authStore = useAuth();
 
-
-    const router = useRouter();
-    const authStore = useAuth();
-
-    const checkAuthByRoute = () => {
-        const secureRoute = ref(['/stock', '/report', '/'])
-        const unsecureRoute = ref(['/login', '/register'])
-        if (secureRoute.value.includes(to.path)) {
-            // secure route
-            if (authStore.session.isLoggedIn) {
-                return
-            } else {
-                console.log('not logged in')
-                return router.push('/login')
-            }
-        } else {
-            // unsecure route
-            if (authStore.session.isLoggedIn && unsecureRoute.value.includes(to.path)) {
-                return router.push('/stock')
-            } else if (authStore.session.isLoggedIn) {
-                return
-            }
-            else {
-                console.log('not logged in')
-                return
-            }
-
-        }
+  // CHECK AUTH BUY LAYOUT
+  authStore.restoreSession();
+  if (to.meta.layout === "default") {
+    // secure
+    if (authStore.session.isLoggedIn) {
+      return;
+    } else {
+      return await navigateTo("/login", { redirectCode: 301 });
     }
-    const checkAuthByLayout = () => {
-        if (to.meta.layout === 'default') {
-            // secure
-            if (authStore.session.isLoggedIn) {
-                return
-            } else {
-                console.log('not logged in')
-                return router.push('/login')
-            }
-        } else {
-            // unsecure
-            if (authStore.session.isLoggedIn) {
-                return router.push('/stock')
-            }
-            else {
-                console.log('not logged in')
-                return
-            }
-        }
+  } else {
+    // unsecure
+    if (authStore.session.isLoggedIn) {
+      return await navigateTo("/stock", { redirectCode: 301 });
+    } else {
+      return;
     }
-
-
-    if (process.client) {
-
-        authStore.restoreSession();
-        if (to.meta.layout === 'default') {
-            // secure
-            if (authStore.session.isLoggedIn) {
-                return
-            } else {
-                return router.push('/login')
-            }
-        } else {
-            // unsecure
-            if (authStore.session.isLoggedIn) {
-                return router.push('/stock')
-            }
-            else {
-                return
-            }
-        }
-
-
-    }
-
-
-
-
-
-
-
-
-
-})
-
-
-
+  }
+});
