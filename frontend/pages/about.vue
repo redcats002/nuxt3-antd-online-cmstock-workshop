57c7f16c1f82691d076e276b4a34772f10a3f03d
redcats002 <redcats_002@hotmail.com>
2023-06-19 18:20:25 +0700

    :sparkles: log test

---
+++ frontend/pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-card>
+    <span class="tw-text-gray-800 tw-font-bold tw-pr-2">{{
+      value.pageView
+    }}</span>
+    <span class="tw-pr-2">Page views since</span>
+    <span class="tw-text-gray-400">{{ time }}</span>
+  </a-card>
+</template>
+<script lang="ts" setup>
+definePageMeta({
+  layout: "default",
+});
+const { data } = await useFetch("/api/pageview");
+const value = data.value as any;
+const time = useTimeAgo(computed(() => value.startAt));
+</script>
